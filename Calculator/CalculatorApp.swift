89a637d8569eba4c765f4b8183f8e6c32199d602
Fabio Luz <108548715+fabio-luz-ey@users.noreply.github.com>
2023-03-27 18:48:34 -0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Luiz Lima on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
